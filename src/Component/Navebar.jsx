38c8c1e7c060e8284beceec7a53d98ae3a774ff1
salman-shan2001 <salmanfarizy454@gmail.com>
2024-06-07 15:07:12 +0530

    updated

--- src/Component/Navebar.jsx
+++ src/Component/Navebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navebar = () => {
   return (
@@ -7,22 +8,22 @@ const Navebar = () => {
         <div className="row">
             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <nav class="navbar navbar-expand-lg bg-warning">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">FISAT LIBRARY</a>
+    <Link class="navbar-brand" to="/">FISAT LIBRARY</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/SearchBook">SEARCH</a>
+          <Link class="nav-link active" aria-current="page" to="/SearchBook">SEARCH</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/DeleteBook">DELETE</a>
+          <Link class="nav-link" to="/DeleteBook">DELETE</Link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/ViewBook">LIBRARY VIEW</a>
+          <Link class="nav-link" to="/ViewBook">LIBRARY VIEW</Link>
         </li>
       </ul>
     </div>
